9fe078ded90f02859802201a9632af1e15e65c1e
Shahid <m.shahidmuneer88@gmail.com>
2019-02-04 13:13:30 +0500

    changed slider.vue

--- components/slider.vue
+++ components/slider.vue
@@ -8,7 +8,7 @@
 			<div class="col-md-12">
 				<!-- Header Contetnt -->
 				<div class="content-block">
-					<h1>Buy & Sell Near You </h1>
+					<h1 style="font-size:45px;">Buy & Sell Near You </h1>
 					<p>Join the millions who buy and sell from each other <br> everyday in local communities around the world</p>
 					<div class="short-popular-category-list text-center">
 						<h2>Popular Category</h2>
